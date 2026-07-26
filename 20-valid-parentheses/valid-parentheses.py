51de999cbfc79a48f6610011054ca18bfa5070ee
AADRIKA SHARMA <aadrikasharma10022004@gmail.com>
2026-07-26 10:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,19 +3,22 @@ class Solution:
         stack = []
         
         for bracket in s:
-
+            # If opening bracket, push to stack
             if bracket in "([{":
                 stack.append(bracket)
             else:
-                if len(stack) == 0 :
+                # For closing bracket, stack cannot be empty
+                if len(stack) == 0:
                     return False
+                
+                # Pop last opening bracket and check if matching
                 ch = stack.pop()
                 if (bracket == ")" and ch == "(") or \
-                        (bracket == "]" and ch == "[") or \
-                        (bracket == "}" and ch == "{"):
+                   (bracket == "]" and ch == "[") or \
+                   (bracket == "}" and ch == "{"):
                     continue
                 else:
                     return False
-
-        return len(stack) == 0
         
+        # Check if all opened brackets were closed
+        return len(stack) == 0
