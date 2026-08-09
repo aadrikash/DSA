d2800db6dec6dc30954910a8b8469101471ff1d4
AADRIKA SHARMA <aadrikasharma10022004@gmail.com>
2026-08-09 09:23:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.py
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.py
@@ -2,27 +2,27 @@ class Solution:
     def findUnion(self, a, b):
         i = 0
         j = 0
-         
         result = []
-        
-        while i < len(a) and j < len(b):
+        while i <len(a) and j < len(b):
             if a[i] <= b[j]:
-                if len(result) == 0 or result[-1] != a[i]:
+                if len(result) == 0 or result[-1]  !=  a[i]:
                     result.append(a[i])
                 i += 1
             else:
                 if len(result) == 0 or result[-1] != b[j]:
                     result.append(b[j])
                 j += 1
+                    
         while i < len(a):
             if len(result) == 0 or result[-1] != a[i]:
                 result.append(a[i])
             i += 1
-            
         while j < len(b):
             if len(result) == 0 or result[-1] != b[j]:
                 result.append(b[j])
             j += 1
-            
         return result 
+            
+        
+        
         
