93028a023c27024b4d281d0d1a5b90418b99f9fd
AADRIKA SHARMA <aadrikasharma10022004@gmail.com>
2026-08-22 23:26:29 +0530

    Time: 19 ms (0.10%) | Memory: 19.1 MB (84.51%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+
+        count = 0
+        maxi = 0
+        for i in range(len(s)):
+            for ch in s:
+                if ch == "(":
+                    count += 1
+                    maxi = max(count,maxi)
+
+                
+                elif ch == ")":
+                    count -= 1
+        
+        return maxi 
+                    
